376cc6e74c17c216b9e90c8806f320d25a8ccc05
Vugar Jabrailzade <vugarej@code.edu.az>
2023-07-06 18:32:17 +0400

    Datetime_Task

---
+++ Date_task2/Date_task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Date_tas
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please add Day: ");
+            var dateDay = Console.ReadLine();
+            int dateDayInt = int.Parse(dateDay);
+
+            Console.WriteLine("Please add Month: ");
+            var dateMonth = Console.ReadLine();
+            var dateMonthInt = Convert.ToInt32(dateMonth);
+
+            Console.WriteLine("Please add Year: ");
+            var dateYear = Console.ReadLine();
+            var dateYearInt = Convert.ToInt32(dateYear);
+
+            DateTime myDate = new DateTime(dateYearInt, dateMonthInt, dateDayInt,00,00,00).AddDays(30).AddHours(6).AddMinutes(17);
+           
+            Console.WriteLine(myDate.ToString("dd/MM/yyyy HH:mm"));
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Task1_Date/Task1_Date/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Globalization;
+using System.Threading.Channels;
+
+namespace Date_tas
+{
+   
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Please enter datetime like this: dd_MM_yyyy: ");
+            
+          
+
+            try
+            {
+               string s = Console.ReadLine();
+               var dt =  DateTime.ParseExact(s, "dd_MM_yyyy", CultureInfo.InvariantCulture);
+                Console.WriteLine(dt);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+
+    }
+
+}
